8e25b846da96dccddb0b3b6f951efc809df8a985
Abel Martell <amartellram@gmail.com>
2022-06-07 23:57:30 -0500

    add array sort example

--- src/main/java/arrayCollectionsDS/arrays/SortingArray.java
+++ src/main/java/arrayCollectionsDS/arrays/SortingArray.java
@@ -1,5 +1,8 @@
 package arrayCollectionsDS.arrays;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class SortingArray {
 
   /**
@@ -8,5 +11,58 @@ public class SortingArray {
    */
   public static void main(String[] args) {
 
+    Melon[] melons = new Melon[]{new Melon("melon 1", 5), new Melon("melon 2", 4)};
+
+    Arrays.sort(melons, new Comparator<Melon>() {
+      @Override
+      public int compare(Melon o1, Melon o2) {
+        return Integer.compare(o1.weight, o2.weight);
+      }
+    });
+
+    print(melons);
+
+    melons = new Melon[]{new Melon("melon 1", 10), new Melon("melon 2", 4),
+        new Melon("melon 3", 8)};
+    Arrays.sort(melons,
+        (melon1, melon2) -> Integer.compare(melon1.getWeight(), melon2.getWeight()));
+
+    print(melons);
+
+
+  }
+
+  static void print(Melon[] a) {
+    for (int i = 0; i < a.length; i++) {
+      System.out.print(a[i] + ", ");
+    }
+    System.out.println();
+  }
+
+  static class Melon {
+
+    private final String type;
+    private final int weight;
+
+    public Melon(String type, int weight) {
+      this.type = type;
+      this.weight = weight;
+    }
+
+    public String getType() {
+      return type;
+    }
+
+    public int getWeight() {
+      return weight;
+    }
+
+    @Override
+    public String toString() {
+      return "Melon{" +
+          "type='" + type + '\'' +
+          ", weight=" + weight +
+          '}';
+    }
   }
 }
